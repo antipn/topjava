953a5b253663c5ef30a8c13ffa2f629b0ec7197c
antipn <palmer@bk.ru>
2022-10-17 19:16:02 +0300

    Hw02 - 1 task Имплементировать InMemoryUserRepository по аналогии с InMemoryMealRepository (список пользователей возвращать отсортированным по имени)

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -6,40 +6,71 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+    //task 1 Имплементировать InMemoryUserRepository по аналогии с InMemoryMealRepository (список пользователей возвращать отсортированным по имени)
 
+    //хранилище в виде мап
+    private final Map<Integer, User> repositoryUser = new ConcurrentHashMap<>();
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    //удаляем пользователя по id с типом ответа boolean
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+        return repositoryUser.remove(id) != null; //успешно удалили true, неуспешно false
     }
 
+    //сохраняем пользователя если он новый, проверяем, если такой по id уже есть просто возвращаем его
     @Override
     public User save(User user) {
         log.info("save {}", user);
+        if (user.isNew()) {
+            user.setId(counter.getAndIncrement());
+            repositoryUser.put(user.getId(), user);
+        }
         return user;
     }
 
+    //получаем одного пользователя по id
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return repositoryUser.get(id);
     }
 
+    //получаем всем пользователей из репо
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        return repositoryUser.values().stream()
+                .sorted(Comparator.comparing((User user) -> user.getName()).thenComparing(user -> user.getEmail())) //специально не буду сокращать тут код чтобы было видно, что иногда важно указывать тип переменной
+                .collect(Collectors.toList());
     }
 
+    //поиск пользователя по email
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return null;
+        //by cycle
+//        User result = null;
+//        for (Map.Entry<Integer, User> entry : repositoryUser.entrySet()) {
+//            if (entry.getValue().getEmail().equals(email)){
+//                result = entry.getValue();
+//            }
+//        }
+//        return result;
+
+        //by stream
+        return repositoryUser.values().stream()
+                .filter((User user) -> user.getEmail().equals(email)).findFirst().orElse(null);
     }
 }
