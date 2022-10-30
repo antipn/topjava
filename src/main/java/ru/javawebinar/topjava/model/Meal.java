d32e8299a910b86226a258164960412ae0e2834d
antipn <palmer@bk.ru>
2022-10-31 00:47:50 +0300

    HW04 prepared Meal for Hibernate + JPA Meal repo done

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,35 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@Entity
+@Table(name = "meals")
 public class Meal extends AbstractBaseEntity {
+    @Column(name = "date_time", nullable = false)
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotBlank
+    @Size(min = 2, max = 120)
     private String description;
 
+    @Column(name = "calories", nullable = false)
+    @NotBlank
+    @Range(min = 10, max = 5000)
     private int calories;
 
+
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false) //указываем имя нашего поля в таблице meals
+    @NotNull
     private User user;
 
     public Meal() {
--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -1,37 +1,59 @@
 package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaMealRepository implements MealRepository {
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
-    public Meal save(Meal meal, int userId) {
-        return null;
+    @Transactional
+    public Meal save(Meal meal, int userId) { // нужно как то сохранить еду переписать сохранение
+        //перед тем как сохранять питание нужно получить его пользователя
+        meal.setUser(em.getReference(User.class, userId));
+        if (meal.isNew()) {
+            em.persist(meal); //сохраняем новое питание
+            return meal;
+        } else if (get((meal.id()), userId) != null) {  // если питание не наше!
+            return null;
+        }
+        return em.merge(meal); //сохраняем через update полей
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return em.createQuery("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userID")
+                .executeUpdate() != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return em.createQuery("SELECT m FROM Meal m WHERE m.id =:id AND m.user.id=:userId", Meal.class)
+                .getSingleResult();
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return em.createQuery("SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC", Meal.class)
+                .getResultList();
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return em.createQuery("SELECT m FROM Meal m WHERE m.user=:userId AND m.dateTime>=:startDateTime AND m.dateTime<:endDateTime ORDER BY m.dateTime DESC", Meal.class)
+                .getResultList();
     }
 }
